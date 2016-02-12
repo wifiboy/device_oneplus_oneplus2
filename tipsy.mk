92faec1d65333d49bf53138d46f894fc2df86730
Martinusbe <martinusbe@gmail.com>
2016-02-12 11:01:45 +0100

    tipsy
    Change-Id: If0ee833e8bcb81bf2b75792187450c211b91f285
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- tipsy.mk
+++ tipsy.mk
@@ -14,8 +14,8 @@
 
 $(call inherit-product, device/oneplus/oneplus2/full_oneplus2.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Tipsy stuff.
+$(call inherit-product, vendor/tipsy/config/common_full_phone.mk)
 
 # Inherit oneplus2-specific vendor tree
 $(call inherit-product-if-exists, vendor/oneplus/oneplus2/oneplus2-vendor.mk)
