d1122916847dbae5bde198a7549c1e9b7f737675
Martinusbe <martinusbe@gmail.com>
2016-05-28 19:22:06 +0200

    Revert "boardconfig:test: no dexpreopt"
    This reverts commit 188c0fc9b83027445a851dfd165dfa8bf92286e2.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -161,6 +161,15 @@ TARGET_RIL_VARIANT := caf
 # RPC
 TARGET_NO_RPC := true
 
+# Enable dexpreopt to speed boot time
+ifeq ($(HOST_OS),linux)
+  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
+  endif
+endif
+
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HAS_QCOM_WLAN_SDK := true
