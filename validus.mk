981fbc4e1a2e2e65e4501671fcfd634fd044a357
Martinusbe <martinusbe@gmail.com>
2016-06-05 00:27:44 +0200

    Validus
    Change-Id: Ie3b2aad095edafc6ce21a25e4e475f6a2aecc48a
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- validus.mk
+++ validus.mk
@@ -14,5 +14,5 @@
 
 $(call inherit-product, device/oneplus/oneplus2/full_oneplus2.mk)
 
-# Inherit some common Tipsy stuff.
-$(call inherit-product, vendor/tipsy/config/common_full_phone.mk)
+# Inherit some common Validus stuff.
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
