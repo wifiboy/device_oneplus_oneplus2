1e888bf5b54c0b416d7a85fc2e50a3b796b806f3
Martinusbe <martinusbe@gmail.com>
2016-06-25 11:31:41 +0200

    oneplus2: disable dexpreopt
    Change-Id: I6dd0bffcb7f45cfc17d70ef3c45c738cf96502fa
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -179,14 +179,8 @@ TARGET_NO_RPC := true
 # Sensors
 USE_SENSOR_MULTI_HAL := true
 
-# Enable dexpreopt to speed boot time
-ifeq ($(HOST_OS),linux)
-  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
-  endif
-endif
+# Dexpreopt
+WITH_DEXPREOPT := false
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
