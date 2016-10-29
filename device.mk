6610e73f2dcde4436dfa3f7bf2c8215c68364b51
fluxi <linflux@arcor.de>
2016-10-29 21:45:27 +0100

    (2/2) oneplus2: Add pocketmode app
    Change-Id: I71e47d5f203adb7e28d37447e19a8041a1d02840

--- device.mk
+++ device.mk
@@ -166,7 +166,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.0-service \
-    fingerprint.msm8994
+    fingerprint.msm8994 \
+    OneplusPocketMode
 
 # For config.fs
 PRODUCT_PACKAGES += \
